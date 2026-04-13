ccca6b882eccf557d5fd5b3c327db881b1819bb4
Alexandre Batisse <alexandre.batisse@scalian.com>
2026-04-13 13:18:33 +0200

    feat: initial project structure

---
+++ src/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+
+print(f"NumPy: {np.__version__}")
+print(f"Pandas: {pd.__version__}")
+print(f"Scikit-learn: {sklearn.__version__}")
+print("✅ Environnement opérationnel")
